17482f44e1a28d882ccd6020adaa5e9500f2bcf7
Daniel Stasinski <daniel@genericinbox.com>
2026-08-18 16:09:04 -0700

    commands: dispatch operator action commands

--- src/commands/dispatch.c
+++ src/commands/dispatch.c
@@ -17,15 +17,16 @@ typedef struct CommandEntry {
 static const CommandEntry command_table[] = {
     {"ADMIN", command_admin}, {"AWAY", command_away}, {"INVITE", command_invite},
     {"ISON", command_ison}, {"JOIN", command_join}, {"KICK", command_kick},
-    {"LIST", command_list}, {"LUSERS", command_lusers}, {"MODE", command_mode},
-    {"MOTD", command_motd}, {"NAMES", command_names}, {"NICK", command_nick},
-    {"NOTICE", command_notice}, {"OPER", command_oper}, {"OPERADD", command_operadd},
-    {"OPERDEL", command_operdel}, {"OPERLIST", command_operlist},
-    {"OPERSET", command_operset}, {"PART", command_part}, {"PASS", command_pass},
-    {"PING", command_ping}, {"PRIVMSG", command_privmsg}, {"QUIT", command_quit},
-    {"RULES", command_rules}, {"TOPIC", command_topic}, {"USER", command_user},
-    {"USERHOST", command_userhost}, {"USERIP", command_userip},
-    {"WHO", command_who}, {"WHOIS", command_whois}
+    {"KILL", command_kill}, {"KLINE", command_kline}, {"LIST", command_list},
+    {"LUSERS", command_lusers}, {"MODE", command_mode}, {"MOTD", command_motd},
+    {"NAMES", command_names}, {"NICK", command_nick}, {"NOTICE", command_notice},
+    {"OPER", command_oper}, {"OPERADD", command_operadd}, {"OPERDEL", command_operdel},
+    {"OPERLIST", command_operlist}, {"OPERSET", command_operset}, {"PART", command_part},
+    {"PASS", command_pass}, {"PING", command_ping}, {"PRIVMSG", command_privmsg},
+    {"QUIT", command_quit}, {"REHASH", command_rehash}, {"RULES", command_rules},
+    {"TOPIC", command_topic}, {"USER", command_user}, {"USERHOST", command_userhost},
+    {"USERIP", command_userip}, {"WALLOPS", command_wallops}, {"WHO", command_who},
+    {"WHOIS", command_whois}, {"ZLINE", command_zline}
 };
 
 CommandResult command_dispatch(Server *server, Client *client,
