c5bf08d87817c0e614d5367a5c1a7320e20ffc2b
Daniel Stasinski <daniel@genericinbox.com>
2026-08-17 23:09:22 -0700

    Implement ADMIN command

---
+++ src/commands/admin.c
@@ -0,0 +1,36 @@
+/**
+ * @file admin.c
+ * @brief Implementation of the IRC ADMIN command.
+ */
+
+#include "commands.h"
+#include "numerics.h"
+
+CommandResult command_admin(Server *server, Client *client, char *params) {
+    (void)params;
+
+    if (command_require_registered(client)) return COMMAND_KEEP_CLIENT;
+
+    if (server->config.admin_location1[0] == '\0' &&
+        server->config.admin_location2[0] == '\0' &&
+        server->config.admin_email[0] == '\0') {
+        client_sendf(client, ERR_NOADMININFO,
+                     server->config.server_name, client->nick,
+                     server->config.server_name);
+        return COMMAND_KEEP_CLIENT;
+    }
+
+    client_sendf(client, RPL_ADMINME,
+                 server->config.server_name, client->nick,
+                 server->config.server_name);
+    client_sendf(client, RPL_ADMINLOC1,
+                 server->config.server_name, client->nick,
+                 server->config.admin_location1);
+    client_sendf(client, RPL_ADMINLOC2,
+                 server->config.server_name, client->nick,
+                 server->config.admin_location2);
+    client_sendf(client, RPL_ADMINEMAIL,
+                 server->config.server_name, client->nick,
+                 server->config.admin_email);
+    return COMMAND_KEEP_CLIENT;
+}
