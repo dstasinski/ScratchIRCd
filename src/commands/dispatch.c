ebc32cdc63fd1bf3ba2d730a49ee93d69d3e90ed
Daniel Stasinski <daniel@genericinbox.com>
2026-08-17 23:03:29 -0700

    Dispatch client state and lookup commands

--- src/commands/dispatch.c
+++ src/commands/dispatch.c
@@ -15,39 +15,25 @@ typedef struct CommandEntry {
 } CommandEntry;
 
 static const CommandEntry command_table[] = {
-    {"NICK",    command_nick},
-    {"USER",    command_user},
-    {"PING",    command_ping},
-    {"JOIN",    command_join},
-    {"PART",    command_part},
-    {"INVITE",  command_invite},
-    {"KICK",    command_kick},
-    {"LIST",    command_list},
-    {"MODE",    command_mode},
-    {"NAMES",   command_names},
-    {"NOTICE",  command_notice},
-    {"PRIVMSG", command_privmsg},
-    {"TOPIC",   command_topic},
-    {"WHO",     command_who},
-    {"WHOIS",   command_whois},
-    {"QUIT",    command_quit}
+    {"NICK", command_nick}, {"USER", command_user}, {"PING", command_ping},
+    {"AWAY", command_away}, {"ISON", command_ison}, {"JOIN", command_join},
+    {"PART", command_part}, {"INVITE", command_invite}, {"KICK", command_kick},
+    {"LIST", command_list}, {"LUSERS", command_lusers}, {"MODE", command_mode},
+    {"NAMES", command_names}, {"NOTICE", command_notice},
+    {"PRIVMSG", command_privmsg}, {"TOPIC", command_topic},
+    {"USERHOST", command_userhost}, {"USERIP", command_userip},
+    {"WHO", command_who}, {"WHOIS", command_whois}, {"QUIT", command_quit}
 };
 
 CommandResult command_dispatch(Server *server, Client *client,
                                const char *command, char *params) {
     size_t index;
 
-    if (server == NULL || client == NULL || command == NULL) {
-        return COMMAND_KEEP_CLIENT;
-    }
-
-    for (index = 0U; index < sizeof(command_table) / sizeof(command_table[0]);
-         ++index) {
-        if (strcasecmp(command, command_table[index].name) == 0) {
+    if (server == NULL || client == NULL || command == NULL) return COMMAND_KEEP_CLIENT;
+    for (index = 0U; index < sizeof(command_table) / sizeof(command_table[0]); ++index) {
+        if (strcasecmp(command, command_table[index].name) == 0)
             return command_table[index].handler(server, client, params);
-        }
     }
-
     client_sendf(client, ERR_UNKNOWNCOMMAND,
                  server->config.server_name, command_reply_nick(client), command);
     return COMMAND_KEEP_CLIENT;
