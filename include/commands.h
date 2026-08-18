cc99304d2e2d867a4e93f764f948636b9676aa48
Daniel Stasinski <daniel@genericinbox.com>
2026-08-17 22:48:19 -0700

    Declare NOTICE TOPIC and KICK handlers

--- include/commands.h
+++ include/commands.h
@@ -22,8 +22,11 @@ CommandResult command_ping(Server *server, Client *client, char *params);
 CommandResult command_join(Server *server, Client *client, char *params);
 CommandResult command_part(Server *server, Client *client, char *params);
 CommandResult command_invite(Server *server, Client *client, char *params);
+CommandResult command_kick(Server *server, Client *client, char *params);
 CommandResult command_mode(Server *server, Client *client, char *params);
+CommandResult command_notice(Server *server, Client *client, char *params);
 CommandResult command_privmsg(Server *server, Client *client, char *params);
+CommandResult command_topic(Server *server, Client *client, char *params);
 CommandResult command_quit(Server *server, Client *client, char *params);
 
 const char *command_reply_nick(const Client *client);
