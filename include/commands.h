ce106aa3e8e86c48fe725a2197644b0963a32518
Daniel Stasinski <daniel@genericinbox.com>
2026-08-17 23:09:38 -0700

    Declare server information commands

--- include/commands.h
+++ include/commands.h
@@ -10,33 +10,36 @@ typedef enum CommandResult {
     COMMAND_DISCONNECT_CLIENT = 1
 } CommandResult;
 
-typedef CommandResult (*CommandHandler)(Server *server, Client *client,
-                                        char *params);
+typedef CommandResult (*CommandHandler)(Server *server, Client *client, char *params);
 
 CommandResult command_dispatch(Server *server, Client *client,
                                const char *command, char *params);
 
-CommandResult command_nick(Server *server, Client *client, char *params);
-CommandResult command_user(Server *server, Client *client, char *params);
-CommandResult command_ping(Server *server, Client *client, char *params);
+CommandResult command_admin(Server *server, Client *client, char *params);
 CommandResult command_away(Server *server, Client *client, char *params);
+CommandResult command_invite(Server *server, Client *client, char *params);
 CommandResult command_ison(Server *server, Client *client, char *params);
 CommandResult command_join(Server *server, Client *client, char *params);
-CommandResult command_part(Server *server, Client *client, char *params);
-CommandResult command_invite(Server *server, Client *client, char *params);
 CommandResult command_kick(Server *server, Client *client, char *params);
 CommandResult command_list(Server *server, Client *client, char *params);
 CommandResult command_lusers(Server *server, Client *client, char *params);
 CommandResult command_mode(Server *server, Client *client, char *params);
+CommandResult command_motd(Server *server, Client *client, char *params);
 CommandResult command_names(Server *server, Client *client, char *params);
+CommandResult command_nick(Server *server, Client *client, char *params);
 CommandResult command_notice(Server *server, Client *client, char *params);
+CommandResult command_part(Server *server, Client *client, char *params);
+CommandResult command_pass(Server *server, Client *client, char *params);
+CommandResult command_ping(Server *server, Client *client, char *params);
 CommandResult command_privmsg(Server *server, Client *client, char *params);
+CommandResult command_quit(Server *server, Client *client, char *params);
+CommandResult command_rules(Server *server, Client *client, char *params);
 CommandResult command_topic(Server *server, Client *client, char *params);
+CommandResult command_user(Server *server, Client *client, char *params);
 CommandResult command_userhost(Server *server, Client *client, char *params);
 CommandResult command_userip(Server *server, Client *client, char *params);
 CommandResult command_who(Server *server, Client *client, char *params);
 CommandResult command_whois(Server *server, Client *client, char *params);
-CommandResult command_quit(Server *server, Client *client, char *params);
 
 const char *command_reply_nick(const Client *client);
 int command_require_registered(Client *client);
