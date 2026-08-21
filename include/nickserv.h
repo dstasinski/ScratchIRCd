08c897b8678e3e731623db4e57b221332516f4ef
Daniel Stasinski <daniel@genericinbox.com>
2026-08-20 19:25:03 -0700

    nickserv: add virtual service interface

---
+++ include/nickserv.h
@@ -0,0 +1,21 @@
+#ifndef IRCD_NICKSERV_H
+#define IRCD_NICKSERV_H
+
+/**
+ * @file nickserv.h
+ * @brief Virtual NickServ service and account-authentication helpers.
+ */
+
+#include "server.h"
+
+/** Process text sent to the virtual NickServ target. */
+void nickserv_handle_message(Server *server, Client *client, char *text);
+
+/** Authenticate a client to a registered account. Returns 1 on success. */
+int nickserv_identify(Server *server, Client *client,
+                      const char *account_name, const char *password);
+
+/** Return non-zero when a nickname is reserved for an internal service. */
+int service_nickname_reserved(const char *nick);
+
+#endif /* IRCD_NICKSERV_H */
