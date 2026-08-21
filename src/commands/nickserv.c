daf34a7a3f72845a43c067c1378e3d717973a18b
Daniel Stasinski <daniel@genericinbox.com>
2026-08-20 20:34:59 -0700

    ircv3: notify peers after direct NickServ account change

--- src/commands/nickserv.c
+++ src/commands/nickserv.c
@@ -8,16 +8,21 @@
  */
 
 #include "commands.h"
+#include "ircv3.h"
 #include "nickserv.h"
 #include "numerics.h"
 
 CommandResult command_nickserv(Server *server, Client *client, char *params) {
+    int was_identified;
     if (command_require_registered(client)) return COMMAND_KEEP_CLIENT;
     if (params == NULL || *params == '\0') {
         client_sendf(client, ERR_NEEDMOREPARAMS,
                      server->config.server_name, client->nick, "NICKSERV");
         return COMMAND_KEEP_CLIENT;
     }
+    was_identified = client->account_name[0] != '\0';
     nickserv_handle_message(server, client, params);
+    if (!was_identified && client->account_name[0] != '\0')
+        ircv3_account_notify(client);
     return COMMAND_KEEP_CLIENT;
 }
