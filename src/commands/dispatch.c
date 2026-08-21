e230bacb4bb6e869da00c67bd76808b5c3b18705
Daniel Stasinski <daniel@genericinbox.com>
2026-08-20 21:22:58 -0700

    commands: dispatch ChanServ and admin commands

--- src/commands/dispatch.c
+++ src/commands/dispatch.c
@@ -3,37 +3,7 @@
 #include "numerics.h"
 #include <stddef.h>
 #include <strings.h>
-
-typedef struct CommandEntry { const char *name; CommandHandler handler; } CommandEntry;
-
-static const CommandEntry command_table[] = {
-    {"ADMIN", command_admin}, {"AUTHENTICATE", command_authenticate}, {"AWAY", command_away},
-    {"CAP", command_cap}, {"CHATHISTORY", command_chathistory}, {"IDENTIFY", command_identify},
-    {"INVITE", command_invite}, {"ISON", command_ison}, {"JOIN", command_join},
-    {"KICK", command_kick}, {"KILL", command_kill}, {"KLINE", command_kline},
-    {"LIST", command_list}, {"LUSERS", command_lusers}, {"MODE", command_mode},
-    {"MOTD", command_motd}, {"NAMES", command_names}, {"NICK", command_nick},
-    {"NICKSERV", command_nickserv}, {"NOTICE", command_notice}, {"NSDROP", command_nsdrop},
-    {"NSINFO", command_nsinfo}, {"NSSET", command_nsset}, {"OPER", command_oper},
-    {"OPERADD", command_operadd}, {"OPERDEL", command_operdel}, {"OPERLIST", command_operlist},
-    {"OPERSET", command_operset}, {"PART", command_part}, {"PASS", command_pass},
-    {"PING", command_ping}, {"PRIVMSG", command_privmsg}, {"QUIT", command_quit},
-    {"REHASH", command_rehash}, {"RESTART", command_restart}, {"RULES", command_rules},
-    {"SAJOIN", command_sajoin}, {"SAMODE", command_samode}, {"SAPART", command_sapart},
-    {"SETHOST", command_sethost}, {"SETIDENT", command_setident}, {"SETNAME", command_setname},
-    {"TOPIC", command_topic}, {"USER", command_user}, {"USERHOST", command_userhost},
-    {"USERIP", command_userip}, {"WALLOPS", command_wallops}, {"WEBIRC", command_webirc},
-    {"WHO", command_who}, {"WHOIS", command_whois}, {"ZLINE", command_zline}
-};
-
-CommandResult command_dispatch(Server *server, Client *client,
-                               const char *command, char *params) {
-    size_t index;
-    if (server == NULL || client == NULL || command == NULL) return COMMAND_KEEP_CLIENT;
-    for (index = 0U; index < sizeof(command_table) / sizeof(command_table[0]); ++index)
-        if (strcasecmp(command, command_table[index].name) == 0)
-            return command_table[index].handler(server, client, params);
-    client_sendf(client, ERR_UNKNOWNCOMMAND,
-                 server->config.server_name, command_reply_nick(client), command);
-    return COMMAND_KEEP_CLIENT;
-}
+typedef struct CommandEntry{const char*name;CommandHandler handler;}CommandEntry;
+static const CommandEntry command_table[]={
+{"ADMIN",command_admin},{"AUTHENTICATE",command_authenticate},{"AWAY",command_away},{"CAP",command_cap},{"CHATHISTORY",command_chathistory},{"CHANSERV",command_chanserv},{"CSDROP",command_csdrop},{"CSINFO",command_csinfo},{"CSSET",command_csset},{"IDENTIFY",command_identify},{"INVITE",command_invite},{"ISON",command_ison},{"JOIN",command_join},{"KICK",command_kick},{"KILL",command_kill},{"KLINE",command_kline},{"LIST",command_list},{"LUSERS",command_lusers},{"MODE",command_mode},{"MOTD",command_motd},{"NAMES",command_names},{"NICK",command_nick},{"NICKSERV",command_nickserv},{"NOTICE",command_notice},{"NSDROP",command_nsdrop},{"NSINFO",command_nsinfo},{"NSSET",command_nsset},{"OPER",command_oper},{"OPERADD",command_operadd},{"OPERDEL",command_operdel},{"OPERLIST",command_operlist},{"OPERSET",command_operset},{"PART",command_part},{"PASS",command_pass},{"PING",command_ping},{"PRIVMSG",command_privmsg},{"QUIT",command_quit},{"REHASH",command_rehash},{"RESTART",command_restart},{"RULES",command_rules},{"SAJOIN",command_sajoin},{"SAMODE",command_samode},{"SAPART",command_sapart},{"SETHOST",command_sethost},{"SETIDENT",command_setident},{"SETNAME",command_setname},{"TOPIC",command_topic},{"USER",command_user},{"USERHOST",command_userhost},{"USERIP",command_userip},{"WALLOPS",command_wallops},{"WEBIRC",command_webirc},{"WHO",command_who},{"WHOIS",command_whois},{"ZLINE",command_zline}};
+CommandResult command_dispatch(Server*s,Client*c,const char*n,char*p){size_t i;if(!s||!c||!n)return COMMAND_KEEP_CLIENT;for(i=0;i<sizeof(command_table)/sizeof(command_table[0]);++i)if(strcasecmp(n,command_table[i].name)==0)return command_table[i].handler(s,c,p);client_sendf(c,ERR_UNKNOWNCOMMAND,s->config.server_name,command_reply_nick(c),n);return COMMAND_KEEP_CLIENT;}
