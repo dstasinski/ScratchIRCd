3a81292f1ecc43edd4fd1ff20c874725b51894ce
Daniel Stasinski <daniel@genericinbox.com>
2026-08-22 16:04:49 -0700

    Bump ScratchIRCd version to 0.32

--- include/config.h
+++ include/config.h
@@ -9,7 +9,7 @@
 
 #define IRCD_DEFAULT_SERVER_NAME "scratch.local"
 #define IRCD_DEFAULT_NETWORK_NAME "ScratchNet"
-#define IRCD_VERSION "ScratchIRCd-0.31"
+#define IRCD_VERSION "ScratchIRCd-0.32"
 #define IRCD_CREATED "August 2026"
 #define IRCD_DEFAULT_PORT "6667"
 #define IRCD_DEFAULT_TLS_PORT "6697"
