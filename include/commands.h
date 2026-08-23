228aef9db4b668be41939ed05c75c69cdfc91565
Daniel Stasinski <daniel@genericinbox.com>
2026-08-22 23:07:50 -0700

    Declare PONG command

--- include/commands.h
+++ include/commands.h
@@ -1,90 +1,14 @@
 #ifndef IRCD_COMMANDS_H
 #define IRCD_COMMANDS_H
-
 #include "server.h"
-
-typedef enum CommandResult {
-    COMMAND_KEEP_CLIENT = 0,
-    COMMAND_DISCONNECT_CLIENT = 1
-} CommandResult;
-
-typedef CommandResult (*CommandHandler)(Server *server, Client *client, char *params);
-
-CommandResult command_dispatch(Server *server, Client *client,
-                               const char *command, char *params);
-CommandResult command_admin(Server *, Client *, char *);
-CommandResult command_authenticate(Server *, Client *, char *);
-CommandResult command_away(Server *, Client *, char *);
-CommandResult command_cap(Server *, Client *, char *);
-CommandResult command_chanserv(Server *, Client *, char *);
-CommandResult command_chathistory(Server *, Client *, char *);
-CommandResult command_csdrop(Server *, Client *, char *);
-CommandResult command_csinfo(Server *, Client *, char *);
-CommandResult command_csset(Server *, Client *, char *);
-CommandResult command_deaf(Server *, Client *, char *);
-CommandResult command_die(Server *, Client *, char *);
-CommandResult command_geoban(Server *, Client *, char *);
-CommandResult command_globops(Server *, Client *, char *);
-CommandResult command_identify(Server *, Client *, char *);
-CommandResult command_invite(Server *, Client *, char *);
-CommandResult command_ison(Server *, Client *, char *);
-CommandResult command_join(Server *, Client *, char *);
-CommandResult command_kick(Server *, Client *, char *);
-CommandResult command_kill(Server *, Client *, char *);
-CommandResult command_kline(Server *, Client *, char *);
-CommandResult command_knock(Server *, Client *, char *);
-CommandResult command_list(Server *, Client *, char *);
-CommandResult command_locops(Server *, Client *, char *);
-CommandResult command_lusers(Server *, Client *, char *);
-CommandResult command_memoserv(Server *, Client *, char *);
-CommandResult command_msinfo(Server *, Client *, char *);
-CommandResult command_mspurge(Server *, Client *, char *);
-CommandResult command_mode(Server *, Client *, char *);
-CommandResult command_motd(Server *, Client *, char *);
-CommandResult command_mute(Server *, Client *, char *);
-CommandResult command_names(Server *, Client *, char *);
-CommandResult command_nick(Server *, Client *, char *);
-CommandResult command_nickserv(Server *, Client *, char *);
-CommandResult command_notice(Server *, Client *, char *);
-CommandResult command_nsdrop(Server *, Client *, char *);
-CommandResult command_nsinfo(Server *, Client *, char *);
-CommandResult command_nsset(Server *, Client *, char *);
-CommandResult command_oper(Server *, Client *, char *);
-CommandResult command_operadd(Server *, Client *, char *);
-CommandResult command_operdel(Server *, Client *, char *);
-CommandResult command_operlist(Server *, Client *, char *);
-CommandResult command_operset(Server *, Client *, char *);
-CommandResult command_part(Server *, Client *, char *);
-CommandResult command_pass(Server *, Client *, char *);
-CommandResult command_ping(Server *, Client *, char *);
-CommandResult command_privmsg(Server *, Client *, char *);
-CommandResult command_quit(Server *, Client *, char *);
-CommandResult command_rehash(Server *, Client *, char *);
-CommandResult command_restart(Server *, Client *, char *);
-CommandResult command_rules(Server *, Client *, char *);
-CommandResult command_sajoin(Server *, Client *, char *);
-CommandResult command_samode(Server *, Client *, char *);
-CommandResult command_sapart(Server *, Client *, char *);
-CommandResult command_sethost(Server *, Client *, char *);
-CommandResult command_setident(Server *, Client *, char *);
-CommandResult command_setname(Server *, Client *, char *);
-CommandResult command_silence(Server *, Client *, char *);
-CommandResult command_topic(Server *, Client *, char *);
-CommandResult command_ungeoban(Server *, Client *, char *);
-CommandResult command_user(Server *, Client *, char *);
-CommandResult command_userhost(Server *, Client *, char *);
-CommandResult command_userip(Server *, Client *, char *);
-CommandResult command_wallops(Server *, Client *, char *);
-CommandResult command_watch(Server *, Client *, char *);
-CommandResult command_webirc(Server *, Client *, char *);
-CommandResult command_who(Server *, Client *, char *);
-CommandResult command_whois(Server *, Client *, char *);
-CommandResult command_whowas(Server *, Client *, char *);
-CommandResult command_zline(Server *, Client *, char *);
-
+typedef enum CommandResult { COMMAND_KEEP_CLIENT = 0, COMMAND_DISCONNECT_CLIENT = 1 } CommandResult;
+typedef CommandResult (*CommandHandler)(Server *, Client *, char *);
+CommandResult command_dispatch(Server *, Client *, const char *, char *);
+#define CMD(name) CommandResult command_##name(Server *, Client *, char *)
+CMD(admin); CMD(authenticate); CMD(away); CMD(cap); CMD(chanserv); CMD(chathistory); CMD(csdrop); CMD(csinfo); CMD(csset); CMD(deaf); CMD(die); CMD(geoban); CMD(globops); CMD(identify); CMD(invite); CMD(ison); CMD(join); CMD(kick); CMD(kill); CMD(kline); CMD(knock); CMD(list); CMD(locops); CMD(lusers); CMD(memoserv); CMD(msinfo); CMD(mspurge); CMD(mode); CMD(motd); CMD(mute); CMD(names); CMD(nick); CMD(nickserv); CMD(notice); CMD(nsdrop); CMD(nsinfo); CMD(nsset); CMD(oper); CMD(operadd); CMD(operdel); CMD(operlist); CMD(operset); CMD(part); CMD(pass); CMD(ping); CMD(pong); CMD(privmsg); CMD(quit); CMD(rehash); CMD(restart); CMD(rules); CMD(sajoin); CMD(samode); CMD(sapart); CMD(sethost); CMD(setident); CMD(setname); CMD(silence); CMD(topic); CMD(ungeoban); CMD(user); CMD(userhost); CMD(userip); CMD(wallops); CMD(watch); CMD(webirc); CMD(who); CMD(whois); CMD(whowas); CMD(zline);
+#undef CMD
 const char *command_reply_nick(const Client *client);
 int command_require_registered(Client *client);
 void command_maybe_register(Server *server, Client *client);
 void command_send_names(Channel *channel, Client *client);
-
 #endif
