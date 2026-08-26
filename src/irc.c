ea6911b8b967d3d726751f2da2f08d12e2ee8174
Daniel Stasinski <daniel@genericinbox.com>
2026-08-25 17:43:10 -0700

    Add client prefix spoof SNOTICE

--- src/irc.c
+++ src/irc.c
@@ -5,6 +5,8 @@
 
 #include "irc.h"
 #include "commands.h"
+#include "message_policy.h"
+#include "oper.h"
 
 #include <string.h>
 
@@ -15,6 +17,9 @@ int irc_handle_line(Server *server, Client *client, char *line) {
 
     if (server == NULL || client == NULL || line == NULL) return 0;
     if (line[0] == ':') {
+        snotice_broadcast(server, SNOTICE_SECURITY,
+                          "Protocol violation: client-supplied prefix from %s [real_ip=%s]",
+                          command_reply_nick(client), client->real_ip);
         client_sendf(client, ":%s ERROR :Client-supplied prefixes are not permitted",
                      server->config.server_name);
         return 1;
