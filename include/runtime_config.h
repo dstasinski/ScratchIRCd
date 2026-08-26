9a6f9857b0a097f0327146183a7855d1ae1707e0
Daniel Stasinski <daniel@genericinbox.com>
2026-08-26 15:10:47 -0700

    Add channel log backlog configuration

--- include/runtime_config.h
+++ include/runtime_config.h
@@ -58,6 +58,7 @@ typedef struct ServerConfig {
     size_t history_limit;
     unsigned int history_retention_days;
     size_t history_max_rows;
+    size_t channel_log_queue_max_rows;
     size_t memoserv_quota;
     unsigned int memoserv_retention_days;
 
