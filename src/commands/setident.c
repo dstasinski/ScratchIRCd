ac1b64a6d101dfff81e53ca6fa9e7baefc45b77a
Daniel Stasinski <daniel@genericinbox.com>
2026-08-25 19:27:01 -0700

    Add SETIDENT identity SNOTICE

--- src/commands/setident.c
+++ src/commands/setident.c
@@ -4,6 +4,7 @@
  */
 
 #include "commands.h"
+#include "message_policy.h"
 #include "numerics.h"
 #include "oper.h"
 
@@ -26,6 +27,7 @@ CommandResult command_setident(Server *server, Client *client, char *params) {
     char *nick;
     char *ident;
     Client *target;
+    char old_ident[IRC_USER_MAX + 1U];
 
     if (command_require_registered(client)) return COMMAND_KEEP_CLIENT;
     if (!oper_permission_has(client->oper_permissions, OPER_PERMISSION_OVERRIDE)) {
@@ -45,10 +47,15 @@ CommandResult command_setident(Server *server, Client *client, char *params) {
         return COMMAND_KEEP_CLIENT;
     }
 
+    (void)snprintf(old_ident, sizeof(old_ident), "%s", target->user);
     (void)snprintf(target->user, sizeof(target->user), "%s", ident);
     client_sendf(client, ":%s NOTICE %s :SETIDENT %s -> %s",
                  server->config.server_name, client->nick, target->nick, target->user);
     client_sendf(target, ":%s NOTICE %s :Your ident is now %s",
                  server->config.server_name, target->nick, target->user);
+    snotice_broadcast(server, SNOTICE_IDENTITY,
+                      "SETIDENT by %s: %s %s -> %s [real_ip=%s]",
+                      client->nick, target->nick, old_ident,
+                      target->user, target->real_ip);
     return COMMAND_KEEP_CLIENT;
 }
