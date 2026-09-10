c4b9c5ad4a0fd7510ffc9c3266387244e2980359
Daniel Stasinski <daniel@genericinbox.com>
2026-09-09 22:33:01 -0700

    Fix command handler typedef

--- include/commands.h
+++ include/commands.h
@@ -2,7 +2,7 @@
 #define IRCD_COMMANDS_H
 #include "server.h"
 typedef enum CommandResult { COMMAND_KEEP_CLIENT = 0, COMMAND_DISCONNECT_CLIENT = 1 } CommandResult;
-typedef CommandHandler (*CommandHandler)(Server *, Client *, char *);
+typedef CommandResult (*CommandHandler)(Server *, Client *, char *);
 CommandResult command_dispatch(Server *, Client *, const char *, char *);
 #define CMD(name) CommandResult command_##name(Server *, Client *, char *)
 CMD(admin); CMD(authenticate); CMD(away); CMD(cap); CMD(chanserv); CMD(chathistory); CMD(csdrop); CMD(csinfo); CMD(csset); CMD(deaf); CMD(die); CMD(eline); CMD(flash); CMD(geoban); CMD(globops); CMD(identify); CMD(info); CMD(invite); CMD(ison); CMD(join); CMD(kick); CMD(kill); CMD(kline); CMD(knock); CMD(links); CMD(list); CMD(locops); CMD(lusers); CMD(memoserv); CMD(msinfo); CMD(mspurge); CMD(mode); CMD(motd); CMD(mute); CMD(names); CMD(nick); CMD(nickserv); CMD(notice); CMD(nsdrop); CMD(nsinfo); CMD(nsset); CMD(oper); CMD(operadd); CMD(operdel); CMD(operlist); CMD(operset); CMD(part); CMD(pass); CMD(ping); CMD(pong); CMD(privmsg); CMD(quit); CMD(rehash); CMD(restart); CMD(rules); CMD(sajoin); CMD(samode); CMD(sapart); CMD(sethost); CMD(setident); CMD(setname); CMD(silence); CMD(snotice); CMD(stats); CMD(tagmsg); CMD(time); CMD(topic); CMD(ungeoban); CMD(user); CMD(userhost); CMD(userip); CMD(version); CMD(wallops); CMD(watch); CMD(webirc); CMD(who); CMD(whois); CMD(whowas); CMD(zline);
