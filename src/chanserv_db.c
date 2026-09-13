060354d66277ff0772aab50be41df3a0d1cb25e8
Daniel Stasinski <daniel@genericinbox.com>
2026-09-13 13:35:08 -0700

    Include logging flag in current ChanServ schema

--- src/chanserv_db.c
+++ src/chanserv_db.c
@@ -157,6 +157,7 @@ static int current_schema_valid(sqlite3 *db) {
         "secure_ops",
         "successor",
         "greeting",
+        "logging_enabled",
         "created_at",
         "updated_at"
     };
@@ -220,6 +221,7 @@ int chanserv_db_open(ChanServDb *db, const char *path) {
         "secure_ops INTEGER NOT NULL DEFAULT 0,"
         "successor TEXT NOT NULL DEFAULT '',"
         "greeting TEXT NOT NULL DEFAULT '',"
+        "logging_enabled INTEGER NOT NULL DEFAULT 0,"
         "created_at INTEGER NOT NULL DEFAULT (unixepoch()),"
         "updated_at INTEGER NOT NULL DEFAULT (unixepoch())"
         ");"
